66eb91ec6b8691c4fcea076be5217f3a3509bc92
Vedant Kumar <111086597+Vedant-K-Kumar@users.noreply.github.com>
2023-10-20 18:29:49 -0400

    Update ColorSensorTest.java
    Made the code run 3 different color tests at the start of the match. Once I figure out the motors and driving, I should be able to program the robot to do different things based on where it senses the color. WIP - lmk if u get errors and send me a pic of them.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
@@ -21,47 +21,31 @@ public class ColorSensorTest extends LinearOpMode {
   /** The colorSensor field will contain a reference to our color sensor hardware object */
   NormalizedColorSensor colorSensor;
 
+  int teamPropPos = 0;
+  double redVal = 0;
+  
   @Override public void runOpMode() {
 
-    try {
-      runSample();
-    } finally {
-      
-    }
-  }
-
-  protected void runSample() {
-    
     colorSensor = hardwareMap.get(NormalizedColorSensor.class, "colorSensor");
 
-    // If possible, turn the light on in the beginning (it might already be on anyway,
-    // we just make sure it is if we can).
     if (colorSensor instanceof SwitchableLight) {
       ((SwitchableLight)colorSensor).enableLight(true);
     }
 
-    // Wait for the start button to be pressed.
     waitForStart();
 
-    // Loop until we are asked to stop
-    while (opModeIsActive()) {
+    runColorTest(1);
+    runColorTest(2);
+    runColorTest(3);
+  }
+  protected void runColorTest(int pos){
       colorSensor.setGain(2);
-
       if (colorSensor instanceof SwitchableLight) {
           SwitchableLight light = (SwitchableLight)colorSensor;
           light.enableLight(!light.isLightOn());
       }
-
       NormalizedRGBA colors = colorSensor.getNormalizedColors();
-
-      /* Use telemetry to display feedback on the driver station. We show the red, green, and blue
-       * normalized values from the sensor (in the range of 0 to 1), as well as the equivalent
-       * HSV (hue, saturation and value) values. See http://web.archive.org/web/20190311170843/https://infohost.nmt.edu/tcc/help/pubs/colortheory/web/hsv.html
-       * for an explanation of HSV color. */
-
-      // Update the hsvValues array by passing it to Color.colorToHSV()
       Color.colorToHSV(colors.toColor(), hsvValues);
-
       telemetry.addLine()
               .addData("Red", "%.3f", colors.red)
               .addData("Green", "%.3f", colors.green)
@@ -70,16 +54,17 @@ public class ColorSensorTest extends LinearOpMode {
               .addData("Hue", "%.3f", hsvValues[0])
               .addData("Saturation", "%.3f", hsvValues[1])
               .addData("Value", "%.3f", hsvValues[2]);
+    
       telemetry.addData("Alpha", "%.3f", colors.alpha);
+      redVal = colors.red;
 
-      /* If this color sensor also has a distance sensor, display the measured distance.
-       * Note that the reported distance is only useful at very close range, and is impacted by
-       * ambient light and surface reflectivity. */
+      /** We don't know how the values will read so we can change this later */
+      if(redVal > 0){
+        teamPropPos = pos;
+      }
       if (colorSensor instanceof DistanceSensor) {
         telemetry.addData("Distance (cm)", "%.3f", ((DistanceSensor) colorSensor).getDistance(DistanceUnit.CM));
       }
-
       telemetry.update();
-    }
   }
 }
